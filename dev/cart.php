227dc359c18c95315164a3c49515e2c84f760a16
MichielN19 <Michielmitsjol@gmail.com>
2025-05-21 19:04:57 +0200

    Added sql file for the entire db setup of the site

--- dev/cart.php
+++ dev/cart.php
@@ -58,8 +58,6 @@ if (!is_null($cart_items)) {
         </div>
       <?php endif; ?>
 
-
-
       <?php foreach ($cart_items as $cart_item):
         $product_id = $cart_item->product_id;
         Database::query("SELECT * FROM products WHERE id = :id", [':id' => $product_id]);
@@ -75,7 +73,7 @@ if (!is_null($cart_items)) {
               <p class="uk-margin-remove-top"> <?= $cart_product->description ?> </p>
             <div class="uk-flex uk-flex-between">
               <p class="uk-text-bolder uk-margin-remove-top">&euro; <?= $cart_product->price?> </p>
-              <p class="uk-text-bolder uk-margin-remove-top">Total price: &euro; <?=$cart_product->price * $cart_item->amount?></p>
+              <p class="uk-text-bolder uk-margin-remove-top">Totale prijs: &euro; <?=$cart_product->price * $cart_item->amount?></p>
             </div>
             </div>
             <div class="uk-width-1-4 uk-flex uk-flex-between uk-flex-middle uk-flex-center">
